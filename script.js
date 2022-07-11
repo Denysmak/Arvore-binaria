a9f3a21432106b3ad4c48d3bfa177579ec5946be
Denysmak <denysmakene13@gmail.com>
2022-07-10 22:37:25 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// preciso que o programa salve 10 numeros
+// criar um array chamado direita
+// criar um array chamado esquerda
+// preciso que o programa pegue o primeiro numero e use como parâmetro
+// os numeros que forem maiores irão para o array direita
+// os numeros que forem iguais ou menores que ele irão para o array direita
+// display o array direita
+// display o array esquerda
